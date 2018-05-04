2b20bca6c4c09f4fb4d36304ac0f59ce4d1d20bf
Marani Matias Ezequiel <maranimatias@gmail.com>
2018-05-03 22:01:59 -0300

    Agrego menu

--- TP1/tp1.pl
+++ TP1/tp1.pl
@@ -7,9 +7,6 @@ desde(ciudad4,ciudad5).
 
 % Rebla permita establecer si es posible llegar desde la ciudad A hasta la ciudad B.
 
-% En caso de no encontrar una conexión.
-conexion(_,_) :-
-  writeln('No existe conexión').
 % Condición de fin
 conexion(A,B) :-
   desde(A,B).
@@ -18,4 +15,19 @@ conexion(A,B) :-
   desde(A,X),
   conexion(X,B).
 
+% En caso de no encontrar una conexión.
+conexion(_,_) :-
+  writeln('No existe conexión').
+
 % Menu
+menu:-
+  write('<-oO Trabajo Practico Oo->'), nl,
+  format('Para salir pulse 0 ~n'),
+  read(OPC), OPC \= 0,
+  write('Ciudad de origen'), nl, read(C1),
+  write('Ciudad de destino'), nl, read(C2),
+  conexion(C1,C2), nl,
+  menu.
+
+menu:-
+  write('adios').
