5975264b111ad8fa935ab70b356d8c88617b2cd1
Marani Matias Ezequiel <maranimatias@gmail.com>
2018-05-03 21:13:48 -0300

    Agrego base de hechos y regla conexion

--- TP1/tp1.pl
+++ TP1/tp1.pl
@@ -1,1 +1,21 @@
- 
+%  Recorridos.
+desde(ciudad1,ciudad2).
+desde(ciudad1,ciudad4).
+desde(ciudad2,ciudad3).
+desde(ciudad4,ciudad6).
+desde(ciudad4,ciudad5).
+
+% Rebla permita establecer si es posible llegar desde la ciudad A hasta la ciudad B.
+
+% En caso de no encontrar una conexión.
+conexion(_,_) :-
+  writeln('No existe conexión').
+% Condición de fin
+conexion(A,B) :-
+  desde(A,B).
+
+conexion(A,B) :-
+  desde(A,X),
+  conexion(X,B).
+
+% Menu
