7f167c1294e1a2935b0a097012a8a094c985096c
Marani Matias Ezequiel <maranimatias@gmail.com>
2018-05-05 00:13:55 -0300

    Basic list

--- TP1/tp1.exs
+++ TP1/tp1.exs
@@ -1,1 +1,18 @@
- 
+destinos = [
+  ciudad1: "ciudad2",
+  ciudad1: "ciudad4",
+  ciudad2: "ciudad3",
+  ciudad4: "ciudad6",
+  ciudad4: "ciudad5",
+  ciudad5: "ciudad6",
+  ciudad2: "ciudad5",
+  ciudad2: "ciudad1"
+]
+
+{:ok, value} = {:ok, "Successful!"} # {:ok, "Successful!"}
+IO.puts value # "Successful!"
+
+for {ciudad_origen, ciudad_destino} <- destinos, ciudad_destino do
+  IO.puts  "Desde #{ciudad_origen} hasta #{ciudad_destino}"
+end
+
