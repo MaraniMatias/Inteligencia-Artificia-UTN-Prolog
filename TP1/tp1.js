251994530c8e5833d5040eefba2caa728d90ee5b
Marani Matias Ezequiel <maranimatias@gmail.com>
2018-05-05 17:13:21 -0300

    Work if not exit loops, from c1 to c2 and from c2 to c1

--- TP1/tp1.js
+++ TP1/tp1.js
@@ -1,36 +1,34 @@
-// ciudad1 to ciduad2 = { ciudad2:'ciudad1' } 
-// destino, orignes[]
-const desde = {
-  ciudad6: ["ciudad5","ciudad4"],
-  ciudad5: ["ciudad2", "ciudad4"], // Pont d
-  ciudad4: ["ciudad1"],
-  ciudad3: ["ciudad2"],
-  ciudad2: ["ciudad1"],
-  ciudad1: []
-}
-
 const recoridos = {
   ciudad1: ["ciudad2", "ciudad4"],
-  ciudad2: ["ciudad3", "ciudad5", "ciudad1"],
+  // ciudad2: ["ciudad3", "ciudad5", "ciudad1"],
+  ciudad2: ["ciudad3", "ciudad5"],
   ciudad4: ["ciudad6", "ciudad5"],
   ciudad5: ["ciudad6"]
 };
 
-/*function conexion(A, B) {
-  return Object.keys(recoridos).find(ruta => {
-      ruta.find(destino => destino === B);
-    });
-}*/
-
 function conexion(A, B) {
-  return desde[B].find(ciudad_origen => ciudad_origen === A ) && "Exite conexion";
+  console.log('Buscar conexion %s con %s', A, B);
+  if (A == B) {
+    return true;
+  }
+  if(typeof recoridos[A] === 'undefined'){
+    return false;
+  }
+
+  // obtengo todos los destionos
+  return recoridos[A].find(
+    // busco un destino que tenga como desino B
+    destino => conexion(destino, B)
+  );
 }
 
+console.log(conexion("ciudad1", "ciudad6") && "Exite conexion"); // falla
+console.log(conexion("ciudad1", "ciudad3") && "Exite conexion"); // falla
 
-console.log(conexion("ciudad1", "ciudad4"));
-console.log(conexion("ciudad1", "ciudad2"));
-console.log(conexion("ciudad1", "ciudad6")); // falla
-console.log(conexion("ciudad2", "ciudad5"));
+console.log(conexion("ciudad1", "ciudad1") && "Exite conexion");
 
-console.log(conexion("ciudad3", "ciudad6"));
+console.log(conexion("ciudad1", "ciudad4") && "Exite conexion");
+console.log(conexion("ciudad1", "ciudad2") && "Exite conexion");
+console.log(conexion("ciudad2", "ciudad5") && "Exite conexion");
 
+console.log(conexion("ciudad3", "ciudad6") || "No exite conexion");
