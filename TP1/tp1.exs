d08e1f1dac97c15f018601505d773ee291c22499
Marani Matias Ezequiel <maranimatias@gmail.com>
2018-05-05 01:07:32 -0300

    Add function

--- TP1/tp1.exs
+++ TP1/tp1.exs
@@ -1,18 +1,32 @@
+# {:ok, "Successful!"}
+{:ok, value} = {:ok, "Successful!"}
+# "Successful!"
+IO.puts(value)
+
 destinos = [
-  ciudad1: "ciudad2",
-  ciudad1: "ciudad4",
-  ciudad2: "ciudad3",
-  ciudad4: "ciudad6",
-  ciudad4: "ciudad5",
-  ciudad5: "ciudad6",
-  ciudad2: "ciudad5",
-  ciudad2: "ciudad1"
+  {:ciudad1, "ciudad2"},
+  {:ciudad1, "ciudad4"},
+  {:ciudad2, "ciudad3"},
+  {:ciudad4, "ciudad6"},
+  {:ciudad4, "ciudad5"},
+  {:ciudad5, "ciudad6"},
+  {:ciudad2, "ciudad5"},
+  {:ciudad2, "ciudad1"}
 ]
 
-{:ok, value} = {:ok, "Successful!"} # {:ok, "Successful!"}
-IO.puts value # "Successful!"
+defmodule Main do
+  def listar_destinos(destinos) do
+    for {ciudad_origen, ciudad_destino} <- destinos, ciudad_destino do
+      IO.puts("Desde #{ciudad_origen} hasta #{ciudad_destino}")
+    end
+  end
 
-for {ciudad_origen, ciudad_destino} <- destinos, ciudad_destino do
-  IO.puts  "Desde #{ciudad_origen} hasta #{ciudad_destino}"
+  def conexion(destinos, {a, b}) do
+    IO.puts("#{a} -> #{b}")
+    %{^a => ciudad_destino} = %{a => destinos[a]}
+    IO.puts(ciudad_destino == b)
+  end
 end
 
+# IO.puts(Main.listar_destinos(destinos))
+IO.puts(Main.conexion(destinos, {:ciudad1, "ciudad2"}))
