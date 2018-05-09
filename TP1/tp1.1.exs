85bf30c385bdc4f548d025fc3435ac6ea4c93c55
Marani Matias Ezequiel <maranimatias@gmail.com>
2018-05-09 02:28:07 -0300

    intento de mejora

--- TP1/tp1.1.exs
+++ TP1/tp1.1.exs
@@ -1,7 +1,7 @@
 recorridos = %{
   ciudad1: [:ciudad2, :ciudad4],
-  ciudad2: [:ciudad3, :ciudad5, :ciudad1],
-  # ciudad2: [:ciudad3, :ciudad5],
+  # ciudad2: [:ciudad3, :ciudad5, :ciudad1],
+  ciudad2: [:ciudad3, :ciudad5],
   ciudad3: [],
   ciudad4: [:ciudad6, :ciudad5],
   ciudad5: [:ciudad6],
@@ -9,33 +9,24 @@ recorridos = %{
 }
 
 defmodule Main do
-  defp buscar_origen(recorridos, {origen, destino}) do
-    IO.puts("Buscar origen para #{origen} y #{destino}")
-
-    {ciudad_orgien, _} =
-      Enum.find(recorridos, {nil, []}, fn ruta ->
-        {_, destinos} = ruta
-
-        Enum.find(destinos, fn destino_nuevo ->
-          destino_nuevo == destino
-        end)
-      end)
-
-    ciudad_orgien
-  end
-
   defp conexion(_, {origen, destino}) when origen == destino do
-    "Con Conexion"
-  end
-
-  defp conexion(_, {origen, destino}) when origen == nil or destino == nil do
-    "Sin Conexion"
+    true
   end
 
   defp conexion(recorridos, {origen, destino}) when origen != destino do
-    ciudad_destino = buscar_origen(recorridos, {origen, destino})
-    IO.puts("Conxion entre #{origen} y #{destino} -> #{ciudad_destino}")
-    conexion(recorridos, {origen, ciudad_destino})
+    exite_conexion = false
+
+    for ciudad <- recorridos[origen] do
+      if !exite_conexion do
+        exite_conexion = conexion(recorridos, {ciudad, destino})
+      end
+
+      exite_conexion
+    end
+
+    IO.puts("conec #{exite_conexion}")
+
+    exite_conexion
   end
 
   def conexion_string(recorridos, {origen, destino}) do
@@ -44,19 +35,18 @@ defmodule Main do
   end
 end
 
-IO.puts(recorridos |> Main.conexion_string({:ciudad3, :ciudad1}))
+IO.puts("1 #{recorridos |> Main.conexion_string({:ciudad3, :ciudad1})}")
 
-# IO.puts(recorridos |> Main.conexion_string({:ciudad1, :ciudad6}))
-
-# IO.puts(recorridos |> Main.conexion_string({:ciudad1, :ciudad1}))
-# IO.puts(recorridos |> Main.conexion_string({:ciudad1, :ciudad2}))
-# IO.puts(recorridos |> Main.conexion_string({:ciudad1, :ciudad3}))
-# IO.puts(recorridos |> Main.conexion_string({:ciudad1, :ciudad4}))
-# IO.puts(recorridos |> Main.conexion_string({:ciudad1, :ciudad5}))
-# IO.puts(recorridos |> Main.conexion_string({:ciudad2, :ciudad1}))
-# IO.puts(recorridos |> Main.conexion_string({:ciudad2, :ciudad5}))
-# IO.puts(recorridos |> Main.conexion_string({:ciudad3, :ciudad2}))
-# IO.puts(recorridos |> Main.conexion_string({:ciudad3, :ciudad6}))
-# IO.puts(recorridos |> Main.conexion_string({:ciudad4, :ciudad1}))
-# IO.puts(recorridos |> Main.conexion_string({:ciudad5, :ciudad2}))
-# IO.puts(recorridos |> Main.conexion_string({:ciudad6, :ciudad1}))
+IO.puts("2 #{recorridos |> Main.conexion_string({:ciudad1, :ciudad6})}")
+IO.puts("3 #{recorridos |> Main.conexion_string({:ciudad1, :ciudad1})}")
+IO.puts("4 #{recorridos |> Main.conexion_string({:ciudad1, :ciudad2})}")
+IO.puts("5 #{recorridos |> Main.conexion_string({:ciudad1, :ciudad3})}")
+IO.puts("6 #{recorridos |> Main.conexion_string({:ciudad1, :ciudad4})}")
+IO.puts("7 #{recorridos |> Main.conexion_string({:ciudad1, :ciudad5})}")
+IO.puts("8 #{recorridos |> Main.conexion_string({:ciudad2, :ciudad1})}")
+IO.puts("9 #{recorridos |> Main.conexion_string({:ciudad2, :ciudad5})}")
+IO.puts("10 #{recorridos |> Main.conexion_string({:ciudad3, :ciudad2})}")
+IO.puts("11 #{recorridos |> Main.conexion_string({:ciudad3, :ciudad6})}")
+IO.puts("12 #{recorridos |> Main.conexion_string({:ciudad4, :ciudad1})}")
+IO.puts("13 #{recorridos |> Main.conexion_string({:ciudad5, :ciudad2})}")
+IO.puts("14 #{recorridos |> Main.conexion_string({:ciudad6, :ciudad1})}")
