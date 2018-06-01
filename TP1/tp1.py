a8e05cd32c1785f5385c6c671bba9649ee329493
Marani Matias Ezequiel <maranimatias@gmail.com>
2018-06-01 00:32:26 -0300

    Fix error for pylint

--- TP1/tp1.py
+++ TP1/tp1.py
@@ -26,18 +26,18 @@ if __name__ == "__main__":
     programa = Programa()
     conexion = programa.conexion
 
-    print '1', conexion("ciudad1", "ciudad1") and "Exite conexion"
-    print '2', conexion("ciudad1", "ciudad2") and "Exite conexion"
-    print '3', conexion("ciudad1", "ciudad3") and "Exite conexion"
-    print '4', conexion("ciudad1", "ciudad4") and "Exite conexion"
-    print '5', conexion("ciudad1", "ciudad5") and "Exite conexion"
-    print '6', conexion("ciudad1", "ciudad6") and "Exite conexion"
-    print '7', conexion("ciudad2", "ciudad1") and "Exite conexion"
-    print '8', conexion("ciudad2", "ciudad4") and "Exite conexion"
-    print '9', conexion("ciudad2", "ciudad5") and "Exite conexion"
-    print '10', conexion("ciudad2", "ciudad6") and "Exite conexion"
-    print '11', conexion("ciudad3", "ciudad2") or "No exite conexion"
-    print '12', conexion("ciudad3", "ciudad6") or "No exite conexion"
-    print '13', conexion("ciudad4", "ciudad1") or "No exite conexion"
-    print '14', conexion("ciudad5", "ciudad2") or "No exite conexion"
-    print '15', conexion("ciudad6", "ciudad1") or "No exite conexion"
+    print( '1', conexion("ciudad1", "ciudad1") and "Exite conexion")
+    print( '2', conexion("ciudad1", "ciudad2") and "Exite conexion")
+    print( '3', conexion("ciudad1", "ciudad3") and "Exite conexion")
+    print( '4', conexion("ciudad1", "ciudad4") and "Exite conexion")
+    print( '5', conexion("ciudad1", "ciudad5") and "Exite conexion")
+    print( '6', conexion("ciudad1", "ciudad6") and "Exite conexion")
+    print( '7', conexion("ciudad2", "ciudad1") and "Exite conexion")
+    print( '8', conexion("ciudad2", "ciudad4") and "Exite conexion")
+    print( '9', conexion("ciudad2", "ciudad5") and "Exite conexion")
+    print( '10', conexion("ciudad2", "ciudad6") and "Exite conexion")
+    print( '11', conexion("ciudad3", "ciudad2") or "No exite conexion")
+    print( '12', conexion("ciudad3", "ciudad6") or "No exite conexion")
+    print( '13', conexion("ciudad4", "ciudad1") or "No exite conexion")
+    print( '14', conexion("ciudad5", "ciudad2") or "No exite conexion")
+    print( '15', conexion("ciudad6", "ciudad1") or "No exite conexion")
