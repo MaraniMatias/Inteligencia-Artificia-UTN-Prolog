76ab91b30bbe78afeb1a83467e8ef8c68d834bbb
Marani Matias Ezequiel <maranimatias@gmail.com>
2018-08-06 18:57:33 -0300

    Add tp2

---
+++ TP2/base-datos.pl
@@ -0,0 +1,22 @@
+%vuelo(origen, destino, tiempo[min])
+vuelo('Salta','Cordoba',80).
+vuelo('Cordoba','Formosa',120).
+vuelo('Cordoba','Rosario',55).
+vuelo('Cordoba','Neuquen',100).
+vuelo('Cordoba','Mendoza',65).
+vuelo('Formosa','Corrientes',50).
+vuelo('Corrientes','Posadas',70).
+vuelo('Corrientes', 'Parana',57).
+vuelo('Corrientes','Santa Fe',93).
+vuelo('Posadas','Parana',85).
+vuelo('Santa Fe','Rosario',30).
+vuelo('Rosario','Parana',25).
+vuelo('Rosario','Buenos Aires',55).
+vuelo('Rosario','Mendoza',175).
+vuelo('Mendoza','Buenos Aires',85).
+vuelo('Mendoza','Neuquen',83).
+vuelo('Neuquen', 'Buenos Aires',90).
+vuelo('Buenos Aires','Calafate',115).
+vuelo('Buenos Aires', 'Puerto Madryn',75).
+vuelo('Neuquen','Calafate',65).
+
