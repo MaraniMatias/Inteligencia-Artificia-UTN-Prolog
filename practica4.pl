937cd91e69cb135119e4c8083878cbb5652afbfb
Marani Matias Ezequiel <maranimatias@gmail.com>
2018-10-04 18:06:07 -0300

    Add exercise 6

--- practica4.pl
+++ practica4.pl
@@ -401,7 +401,20 @@ count_book(Count) :-
 count_book(0) :-
   open_libreriaDB.
 
-% ------------------------ %
+% 6. Hacer un programa que permita registrar en una Base de Datos recetas
+% de cocina. De cada receta se registran los siguientes datos:
+%       • Código de receta
+%       • Nombre de la receta
+% Y por cada ingrediente que contenga la receta:
+%       • Nombre del ingrediente
+%       • Cantidad
+% A su vez, permitir ingresar dos (2) ingredientes e informar de todas las
+% recetas (Código y Nombre) que poseen ambos ingredientes.
+% Por otro lado, para un ingrediente en particular y una cierta cantidad del
+% mismo, determinar aquellas recetas que llevan ese ingrediente y superan
+% dicha cantidad.
+
+% ------------------------------------------------------------------------ %
 % Para tener todas la DB cargas desdel el principo
 openAllDB :-
   writeln('Open all DB...'),
