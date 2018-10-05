435c10a6e09f47fac40301e9fabdbb4370dc8ef0
Marani Matias Ezequiel <maranimatias@gmail.com>
2018-10-05 06:58:02 -0300

    Add practice three

---
+++ practica3.pl
@@ -0,0 +1,79 @@
+% 1. Ingresar una cadena de texto y obtener el primer carácter de la misma.
+ej_1 :-
+  write('Ingrese una cadena: '),
+  read(Cadena),
+  sub_atom(Cadena, 0, 1, _, C),
+  write('Cadena resultante: '),
+  write(C).
+
+% 2. Ingresar una cadena de texto y obtener el último carácter de la misma.
+ej_2 :-
+  write('Ingrese cadena: '),
+  read(Cadena),
+  atom_length(Cadena, A),
+  R is A - 1,
+  sub_atom(Cadena, R, 1, _, C),
+  write(C).
+
+% 3. Ingresar una cadena de texto e informar cuántos caracteres tiene.
+% En primer lugar haciendo uso del predicado atom_length/2 y en una
+% segunda instancia sin hacer uso de dicho predicado.
+ej_3 :-
+  read(C),
+  longitud(C, Long),
+  write(Long).
+
+longitud('', 0).
+longitud(C, Long) :-
+  sub_atom(C, 0, 1, R, _),
+  sub_atom(C, 1, R, _, C1),
+  longitud(C1, L1),
+  Long is L1 + 1.
+
+% TODO
+% 4. Transformar una cadena en una lista de caracteres.
+
+% 5. Transformar una cadena de texto en una lista de palabras, tomando
+% como divisor el espacio
+ej_5 :-
+  read(C),
+  transformar(C, Lista),
+  writeln(Lista).
+
+transformar(C, [H|T]) :-
+  sub_atom(C, PosEspacio, _, _, ' '),
+  sub_atom(C, 0, PosEspacio, _, H),
+  P is PosEspacio + 1,
+  sub_atom(C, P, _, 0, C1),
+  transformar(C1, T).
+transformar(P, [P]).
+
+% TODO
+% 6. Hacer un programa que transforme un número entero a binario.
+% TODO
+% 7. Hacer un reconocedor de palabras de la forma a n b n .
+
+% 8. Ingresar una cadena y un carácter, luego informar la cantidad de veces
+% que aparece dicho carácter en la cadena.
+ej_8 :-
+  read(C),
+  read(L),
+  contar(C, L, Cont),
+  write(Cont).
+
+contar('', _, 0).
+contar(C, L, Cont) :-
+  sub_atom(C, 0, 1, R, L),
+  sub_atom(C, 1, R, _, C1),
+  contar(C1, L, Cont1),
+  Cont is Cont1 + 1.
+contar(C, L, Cont) :-
+  sub_atom(C, 0, 1, R, _),
+  sub_atom(C, 1, R, _, C1),
+  contar(C1, L, Cont).
+
+% TODO
+% 9. Ingresar una cadena, contar e informar el número de veces que aparece
+% cada una de las vocales (a, e, i, o, u) y la cantidad de veces que
+% aparece cualquier consonante.
+
