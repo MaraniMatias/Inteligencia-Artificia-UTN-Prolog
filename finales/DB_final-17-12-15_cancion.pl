c350f403894f27e8b503008a408d6c4844fadc95
Marani Matias Ezequiel <maranimatias@gmail.com>
2018-10-11 21:17:11 -0300

    Add final-17-12-15

---
+++ finales/DB_final-17-12-15_cancion.pl
@@ -0,0 +1,9 @@
+:- dynamic cancion/5.
+
+% cancion(IdCancion, Nombre, Artista, Duración, Genero)
+cancion(01, 'Nombre vals XL', 'Mac Art',  524, vals).
+cancion(02, 'Nombre vals',    'Mac Art',  124, vals).
+cancion(04, 'Nombre c4',      'Kudo Art', 120, jazz).
+cancion(05, 'Nombre c5',      'Kudo Art', 521, blues).
+cancion(06, 'Nombre c6',      'Unix Art', 16,  bit).
+cancion(07, 'Nombre c7',      'LoHondo',  328, mix).
---
+++ finales/DB_final-17-12-15_invitados.pl
@@ -0,0 +1,13 @@
+:- dynamic invitado/2.
+
+% invitados(Nombre, [IdCanciones que gusta])
+invitado(ai,       [04,05,02]).
+invitado(ayumi,    [04,05,02]).
+invitado(cesar,    [04,05,06]).
+invitado(conan,    [04,05,06]).
+invitado(kaito,    [04,05,07]).
+invitado(matias,   [04,05,01]).
+invitado(noemi,    [04,05,01]).
+invitado(romina,   [04,05,03]).
+invitado(rum,      [02,05,03]).
+invitado(shinichi, [02,05,07]).
